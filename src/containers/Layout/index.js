36a3190728cf7f06e2b7b3025192a02e90362950
Hung Tran <hungtran0203@gmail.com>
2018-07-14 16:03:20 +0700

    organize code

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -8,6 +8,7 @@ import { fromJS } from 'immutable';
 import uuid from 'uuid';
 import invariant from 'invariant';
 import _ from 'lodash';
+import * as builder from 'libs/builder';
 
 const { Flex, Box } = Components;
 
@@ -16,7 +17,7 @@ export const Layout = ({ doPush, doInsert, newRoot, rootItem }) => {
   return (
     <div  className={classnames(styles.container)}>
       <Flex>
-        {rootItem && buildItem(rootItem)}
+        {rootItem && builder.itemBuilder(rootItem)({ onClick })}
       </Flex>
       <Flex className={styles.actions} justify="space-between">
         <Box w={100} className={styles.btn} onClick={newRoot}>New</Box>
@@ -31,46 +32,14 @@ const DATA_STREAM = 'data.stream';
 const ROOT_ITEM_STREAM = 'data.root.stream';
 const ACTIVE_ITEM_STREAM = 'activeItem.stream';
 
-const onClick = (index) => (e) => {
+const onClick = builder.createFactory((item) => (e) => {
   // e.preventDefault();
   e.stopPropagation();
-  getStream(ACTIVE_ITEM_STREAM).set(index);
+  console.log('click me', item, item.toJS());
+  getStream(ACTIVE_ITEM_STREAM).set(item);
   return false;
-};
+});
 
-
-const buildItem = (item) => {
-  const itemObj = isBoxId(item) ? getBoxFromId(item) : item;
-  const { type, id, parentId, children, ...rest } = itemObj.toJS();
-  const Com = Components[type];
-  let subItems;
-  if(children && children.length) {
-    subItems = buildItems(children);
-  }
-  if(subItems) {
-    return <Com {...rest} onClick={onClick(id)} >{subItems}</Com>;
-  }
-  return <Com {...rest} onClick={onClick(id)} />;
-}
-
-const buildItems = (items) => {
-  return items.map((item) => {
-    const { type, id, parentId, children, ...rest } = item;
-    const Com = Components[type];
-    let subItems;
-    if(children && children.length) {
-      subItems = buildItems(children);
-    }
-    if(subItems) {
-      return <Com {...rest} key={id} onClick={onClick(id)} >{subItems}</Com>;
-    }
-    return <Com {...rest} key={id} onClick={onClick(id)} />;
-  })
-}
-
-export const isBoxId = (boxId) => {
-  return typeof boxId === 'string';
-}
 
 export const initBox = ({ parentId }) => {
   return fromJS({
@@ -84,75 +53,53 @@ export const initBox = ({ parentId }) => {
 };
 
 export const newRoot = (opts) => {
-  const data$ = getDataStream()
   const newBox = initBox({ ...opts, parentId: null });
-  data$.set(data$.get().set(newBox.get('id'), newBox));
+  builder.updateItem(newBox);
   return newBox;
 };
 
-const initData = fromJS({});
-export const getDataStream = () => getStream(DATA_STREAM, { init: initData });
-
-export const insertBox = (parentBox, opts) => {
-  const data$ = getDataStream();
-  const parentId = isBoxId(parentBox) ? parentBox : _.get(parentBox, 'id');
-  const data = data$.get();
-  const parent = data.get(parentId);
-  invariant(parent, `Parent Box (${parentId}) does not exists`);
+export const insertBox = (parentItem, opts) => {
+  const parent = builder.isItemId(parentItem) ? builder.getItemFromId(parentItem) : parentItem;
+  invariant(parent, `Parent Box (${parentItem}) does not exists`);
+  const parentId = builder.isItemId(parentItem) ? parentItem : parent.get('id');
   const newBox = initBox({ ...opts, parentId });
-  const children = parent.get('children', fromJS([])).push(newBox);
-  data$.set(data.asMutable()
-    .set(newBox.get('id'), newBox)
-    .setIn([parentId, 'children'], children)
-    .asImmutable()
-  );
+  const children = parent.get('children', fromJS([])).push(builder.getItemId(newBox));
+  builder.updateItem(newBox);
+  const newParent = parent.set('children', children);
+  builder.updateItem(newParent);
+  return [newBox, newParent];
 };
-
-export const getBoxFromId = (boxId, toJS=0) => {
-  const data$ = getDataStream();
-  const box = data$.get().get(boxId);
-  invariant(box, `Box (${boxId}) does not exists`);
-  return toJS ? box.toJS() : box;
-}
-
 
 export default compose(
   withStreamProps({
-    data: [DATA_STREAM, { init: initData }],
-    rootId: [ROOT_ITEM_STREAM],
+    rootItem: [ROOT_ITEM_STREAM],
   }),
   withStreams({
-    rootId$: [ROOT_ITEM_STREAM],
-    activeItem$: [ACTIVE_ITEM_STREAM, { init: 0 }],
+    rootItem$: [ROOT_ITEM_STREAM, { init: null }],
+    activeItem$: [ACTIVE_ITEM_STREAM, { init: null }],
   }),
   withHandlers({
-    setRootId: ({ rootId$ }) => (rootId) => rootId$.set(rootId),
-    setActiveId: ({ activeItem$ }) => (activeId) => activeItem$.set(activeId),
+    setRootItem: ({ rootItem$ }) => (rootItem) => rootItem$.set(rootItem),
+    setActiveItem: ({ activeItem$ }) => (activeItem) => activeItem$.set(activeItem),
   }),
   withHandlers({
-    newRoot: ({ setRootId, setActiveId }) => () => {
+    newRoot: ({ setRootItem, setActiveItem }) => () => {
       const root = newRoot();
-      setRootId(root.get('id'));
-      setActiveId(root.get('id'));
+      setRootItem(root);
+      setActiveItem(root);
       return root;
     },
     doPush: ({ activeItem$ }) => () => {
-      console.log('dopush', activeItem$.get());
-      const activeId = activeItem$.get() || 0;
-      console.log('aaa', activeId);
-      const activeBox = getBoxFromId(activeId, true);
-      console.log('aaa', activeBox);
-      const parentBox = getBoxFromId(activeBox.parentId, true);
-      insertBox(parentBox.id, {});
+      const activeItem = activeItem$.get();
+      if(activeItem && activeItem.get('parentId')) {
+        const parentBox = builder.getItemFromId(activeItem.get('parentId'), true);
+        insertBox(parentBox.id, {});
+      }
     },
     doInsert: ({ activeItem$ }) => () => {
-      const activeId = activeItem$.get() || 0;
-      console.log('activeIdactiveIdactiveId', activeId);
-      insertBox(activeId, {});
+      const activeItem = activeItem$.get();
+      const [newItem, newParent ] = insertBox(activeItem, {});
+      activeItem$.set(newItem);
     }
-  }),
-  withProps(({ rootId, data }) => {
-    console.log('rrrr', rootId, data);
-    return {rootItem: !!rootId ? getBoxFromId(rootId) : null };
   }),
 )(Layout);
